2ab0c4a005f926a79e0f0458087ce8c917097d56
Samosa12345 <142589207+Samosa12345@users.noreply.github.com>
2023-09-02 08:56:59 +0530

    commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -19,7 +19,7 @@ async def start(client, message):
         buttons = [[           
             InlineKeyboardButton('📢 Uᴩᴅᴀᴛᴇꜱ 📢', url=f'https://t.me/{SUPPORT_CHAT}')
             ],[
-            InlineKeyboardButton('ℹ️ Hᴇʟᴩ ℹ️', url=f"https://t.me/{temp.U_NAME}?start=help")
+            InlineKeyboardButton('ℹ️ How To Download ℹ️', url=f"https://t.me/Public_Movie_Support/4")
         ]]
         await message.reply(START_MESSAGE.format(user=message.from_user.mention if message.from_user else message.chat.title, bot=client.mention), reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)                    
         await asyncio.sleep(2) 
